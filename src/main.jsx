6a2f1cc10fc520186e3fa24bcfafb9eb99f7a342
Martin Novák <martin.novak@argo22.com>
2022-12-30 10:30:13 +0100

    Import
    - added confetti import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 
 import "./index.css";
 import "animate.css";
+import "./utils/confetti.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
